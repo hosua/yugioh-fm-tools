1a5de1a719419b3959f33a0239601408e8119fdc
hosua <hosua@gmail.com>
2023-09-04 11:16:44 -0400

    Card Drop Table almost done, but has glaring issue.
    The table won't fucking re-render when a new duelist is selected. WHYYYY
    
    Maybe it's time to start using classes instead of functions.

--- db-server/server.js
+++ db-server/server.js
@@ -27,7 +27,7 @@ app.post("/yugioh-fm-tools/api/fetch-card-list", (req, res) => {
 });
 
 app.post("/yugioh-fm-tools/api/fetch-duelists", (req, res) => {
-    console.log("Fetching card drops...");
+    console.log("Fetching duelists...");
     var query = `SELECT Name FROM ${db_tables.duelist}`;
 
     connection.query(query, (err, results, fields) => {
@@ -44,15 +44,16 @@ app.post("/yugioh-fm-tools/api/fetch-duelists", (req, res) => {
 
 app.post("/yugioh-fm-tools/api/fetch-card-drops", (req, res) => {
     console.log("Fetching card drops...");
-    var query = `SELECT * FROM WHERE Duelist=${req.duelist} ${db_tables.card_drop}`;
 
-    connection.query(query, (err, results, fields) => {
+    var query = `SELECT * FROM ${db_tables.card_drop} WHERE Duelist = ?`;
+
+    const { queryFilter } = req.body;
+
+    connection.query(query, [queryFilter], (err, results, fields) => {
         if (err) {
-            console.log(err);
             connection.end();
             return res.status(500).json({ error: "Error fetching data" });
         }
-
         const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
         res.json(data);
     });
--- src/YFMDatabase.js
+++ src/YFMDatabase.js
@@ -4,18 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './YFM.css'
 
-/* Parameters for YFMCardListTable
- * header="your header"
- * subheader="your subheader" (optional)
- * post='/path/to/post'
- */
-function YFMCardListTable(props) {
-    const [data, setData] = useState([]); // data of the table
+function BootstrapTable(props) {
+    const [data, setData] = useState(props.data || []); // data of the table
     const [filteredData, setFilteredData] = useState([]); // the filtered table
     const [dataFilter, setDataFilter] = useState(''); // string to filter by
     const [sortCol, setSortCol] = useState(null);
     const [sortDir, setSortDir] = useState('asc');
 
+    const [queryFilter, setQueryFilter] = useState(props.filter)
 
     // Since re-rendering the table every onChange event is slow, we want to add
     // a delay from when the user stops typing before the render should occur.
@@ -25,16 +21,18 @@ function YFMCardListTable(props) {
 
         // Function to update filtered data after a delay
         const updateFilteredData = () => {
-            setFilteredData(
-                data.filter((dt) =>
-                    // Check if any column contains the filter text
-                    Object.values(dt).some((val) => {
-                        return (val) ?
-                            val.toString().toLowerCase().includes(dataFilter.toLowerCase()) :
-                            false;
-                    })
-                )
-            );
+            if (Array.isArray(data)) {
+                setFilteredData(
+                    data.filter((dt) =>
+                        // Check if any column contains the filter text
+                        Object.values(dt).some((val) => {
+                            return (val) ?
+                                val.toString().toLowerCase().includes(dataFilter.toLowerCase()) :
+                                false;
+                        })
+                    )
+                );
+            }
         };
 
         // Clear previous timeout (if any) and start a new one
@@ -44,36 +42,6 @@ function YFMCardListTable(props) {
         // Clean up the timeout on unmount
         return () => clearTimeout(timeoutId);
     }, [dataFilter, data]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('yugioh-fm-tools/api/fetch-card-list', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({}),
-                });
-
-                if (response.ok) {
-                    const fetched_data = await response.json();
-                    setData(fetched_data)
-                } else {
-                    console.error('Failed to fetch data');
-                }
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-        fetchData();
-
-    }, []); // Empty dependency array to run the effect only once after the initial render
-
-    // Fail-safe: Show a loading message or component when data is not loaded
-    if (data.length === 0) {
-        return <p>Loading data...</p>;
-    }
 
     const handleDataFilter = (filter) => {
         setDataFilter(filter);
@@ -168,46 +136,12 @@ function YFMCardListTable(props) {
     );
 }
 
-function YFMCardDropTable(props) {
-    const [data, setData] = useState([]); // data of the table
-    const [filteredData, setFilteredData] = useState([]); // the filtered table
-    const [dataFilter, setDataFilter] = useState(''); // string to filter by
-    const [sortCol, setSortCol] = useState(null);
-    const [sortDir, setSortDir] = useState('asc');
-
-
-    // Since re-rendering the table every onChange event is slow, we want to add
-    // a delay from when the user stops typing before the render should occur.
-    useEffect(() => {
-        // Define a timer to delay filtering
-        let timeoutId;
-
-        // Function to update filtered data after a delay
-        const updateFilteredData = () => {
-            setFilteredData(
-                data.filter((dt) =>
-                    // Check if any column contains the filter text
-                    Object.values(dt).some((val) => {
-                        return (val) ?
-                            val.toString().toLowerCase().includes(dataFilter.toLowerCase()) :
-                            false;
-                    })
-                )
-            );
-        };
-
-        // Clear previous timeout (if any) and start a new one
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(updateFilteredData, 250); // delay (in ms)
-
-        // Clean up the timeout on unmount
-        return () => clearTimeout(timeoutId);
-    }, [dataFilter, data]);
-
+function YFMDatabaseCardList() {
+    const [data, setData] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(props.post, {
+                const response = await fetch('yugioh-fm-tools/api/fetch-card-list', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -217,6 +151,7 @@ function YFMCardDropTable(props) {
 
                 if (response.ok) {
                     const fetched_data = await response.json();
+                    console.log(`card list data: ${JSON.stringify(fetched_data)}`)
                     setData(fetched_data)
                 } else {
                     console.error('Failed to fetch data');
@@ -234,101 +169,22 @@ function YFMCardDropTable(props) {
         return <p>Loading data...</p>;
     }
 
-    const handleDataFilter = (filter) => {
-        setDataFilter(filter);
-    };
-
-    const handleSort = (col) => {
-        if (sortCol === col) {
-            // Toggle the sort direction
-            setSortDir(sortDir === 'asc' ? 'desc' : 'asc');
-        } else {
-            // Set a new sort column in ascending direction
-            setSortCol(col);
-            setSortDir('asc');
-        }
-    };
-
-    const sortedData = [...filteredData].sort((a, b) => {
-        const colA = a[sortCol];
-        const colB = b[sortCol];
-
-        // Handle null values
-        if (colA === null) {
-            return sortDir === 'asc' ? -1 : 1;
-        }
-        if (colB === null) {
-            return sortDir === 'asc' ? 1 : -1;
-        }
-        // ascending
-        if (sortDir === 'asc') {
-            return (colA < colB) ? -1 : 1;
-        }
-        // descending
-        return (colA > colB) ? -1 : 1;
-    });
-
-    // Render the table headers and rows only when data is available
-    const columnHeaders = Object.keys(data[0]);
-    const tableHeaders = columnHeaders.map((col) => (
-        <th key={col}>
-            {col}
-            <Button
-                variant="link"
-                onClick={() => handleSort(col)}
-                style={{ width: '20px' }}
-            >
-                {sortCol === col ? (
-                    // The active col
-                    <i className={`fa fa-sort-${sortDir === 'asc' ? 'up' : 'down'}`}></i>
-                ) : (
-                    // The inactive col
-                    <i className={`sort-button fa fa-sort`}></i>
-                )
-                }
-            </Button>
-        </th >
-    ));
-
-    const tableRows = sortedData.map((entry, index) => (
-        <tr key={index}>
-            {columnHeaders.map((key) => (<td key={key}>{entry[key]}</td>
-            ))}
-        </tr>
-    ));
-
     return (
         <>
-            <h1>{props.header}</h1>
-            <hr />
-            <h2>{props.subheader}</h2>
-            <Container data-bs-theme="dark">
-                <FloatingLabel label="Search">
-                    <Form.Control
-                        style={{ width: '200px' }}
-                        type="text"
-                        value={dataFilter}
-                        onChange={(e) => handleDataFilter(e.target.value)}
-                    />
-                </FloatingLabel>
-                <Table striped bordered hover data-bs-theme="dark">
-                    <thead>
-                        <tr>
-                            {tableHeaders}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tableRows}
-                    </tbody>
-                </Table>
-            </Container >
+            <BootstrapTable
+                header="Yu-Gi-Oh Forbidden Memories Database"
+                subheader="Card List"
+                data={data}
+            />
         </>
     );
 }
 
-function YFMSelectDuelist() {
-    const itemsPerRow = 4;
+function YFMDatabaseCardDrops() {
     const [duelists, setDuelists] = useState([]);
+    const [data, setData] = useState([]);
+    const [selectedDuelist, setSelectedDuelist] = useState('');
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -352,56 +208,82 @@ function YFMSelectDuelist() {
             }
         };
         fetchData();
-
     }, []); // Empty dependency array to run the effect only once after the initial render
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('yugioh-fm-tools/api/fetch-card-drops', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        queryFilter: selectedDuelist,
+                    }),
+                });
+
+                if (response.ok) {
+                    const fetched_data = await response.json();
+                    console.log(`card drops data: ${JSON.stringify(fetched_data)}`)
+                    setData(fetched_data)
+                } else {
+                    console.error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+        if (selectedDuelist) {
+            fetchData();
+            setCount(count + 1);
+            console.log(`count: ${count}`)
+        }
+
+    }, [selectedDuelist]);
+    // NOTE: Very important to understand here: putting selectedDuelist here
+    // means that this useEffect() will occur when selectedDuelist is changed! 
+
+    const handleDuelistSelect = (duelist) => {
+        console.log(`Selected Duelist: ${duelist}`)
+        setSelectedDuelist(duelist);
+    };
 
     // Fail-safe: Show a loading message or component when data is not loaded
     if (duelists.length === 0) {
-        return <p>Loading data...</p>;
+        return <p>Loading duelists...</p>;
     }
 
-    console.log(JSON.stringify(duelists));
-
-    return (
-        <Container data-bs-theme="dark">
-            <h2 style={{ textAlign: 'center' }}>Select Duelist</h2>
-            <ButtonGroup className="d-flex flex-wrap">
-                {duelists.map((entry, index) => (
-                    <Button
-                        key={index}
-                        variant="outline-light"
-                        style={{ borderRadius: '0' }}>
-                        {entry.Name}
-                    </Button>
-                ))}
-            </ButtonGroup>
-        </Container>
-    );
-}
-
-function YFMDatabaseCardList() {
     return (
         <>
-            <YFMCardListTable
-                header="Yu-Gi-Oh Forbidden Memories Database"
-                subheader="Card List"
-            />
-        </>
-    );
-}
-
-function YFMDatabaseCardDrops() {
-
-    return (
-        <>
-            <YFMSelectDuelist />
-            {/*
-            <YFMCardDropTable
-                header="Yu-Gi-Oh Forbidden Memories Database"
-                subheader="Card Drops"
-                duelist=''
-            />
-            */}
+            <Container data-bs-theme="dark">
+                <h1>Card Drops List</h1>
+                <h2 style={{ textAlign: 'center' }}>Select a Duelist</h2>
+                <ButtonGroup className="d-flex flex-wrap">
+                    {duelists.map((entry, index) => (
+                        <Button
+                            key={index}
+                            variant="outline-light"
+                            style={{ borderRadius: '0' }}
+                            onClick={() => handleDuelistSelect(entry.Name)}
+                        >
+                            {entry.Name}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            </Container>
+            <Container>
+                {data.length === 0 ? (
+                    null
+                ) : (
+                    <BootstrapTable
+                        header="Yu-Gi-Oh Forbidden Memories Database"
+                        subheader="Card Drops"
+                        data={data}
+                    />
+                )}
+            </Container>
         </>
     );
 }
