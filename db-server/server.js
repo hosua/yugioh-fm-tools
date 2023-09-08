e570c1462b579e937048add4a068a99535bf7534
hosua <hosua@gmail.com>
2023-09-08 15:45:06 -0400

    The err -104 was not due to connection timeouts. Since I cannot for the life of me figure out how to debug this, I'm just going to use the pm2 module which will restart the server every time it throws an uncaught exception. Jank, but should be foolproof I guess.

--- db-server/server.js
+++ db-server/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const { connection, db_tables } = require('./db-config.js');
-const timeout = require('connect-timeout');
-
-const TIMEOUT = '60s';
 
 const app = express();
 app.use(express.json());
-app.use(timeout(TIMEOUT));
 
 connection.connect((err) => {
 	if (err) throw err;
@@ -15,59 +11,47 @@ connection.connect((err) => {
 
 // get messages from server
 app.post("/yugioh-fm-tools/api/fetch-card-list", (req, res) => {
-	if (!req.timedout){
-		console.log("Fetching card list...");
-		var query = `SELECT * FROM ${db_tables.card_list}`;
-		connection.query(query, (err, results, fields) => {
-			if (err) {
-				console.log(err);
-				connection.end();
-				return res.status(500).json({ error: "Error fetching data" });
-			}
+	console.log("Fetching card list...");
+	var query = `SELECT * FROM ${db_tables.card_list}`;
+	connection.query(query, (err, results, fields) => {
+		if (err) {
+			console.log(err);
+			connection.end();
+			return res.status(500).json({ error: "Error fetching data" });
+		}
 
-			const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
-			res.json(data);
-		});
-	} else {
-		console.log("fetch-card-list timed out!");
-	}
+		const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
+		res.json(data);
+	});
 });
 
 app.post("/yugioh-fm-tools/api/fetch-duelists", (req, res) => {
-	if (!req.timedout){
-		console.log("Fetching duelists...");
-		var query = `SELECT Name FROM ${db_tables.duelist}`;
-		connection.query(query, (err, results, fields) => {
-			if (err) {
-				console.log(err);
-				connection.end();
-				return res.status(500).json({ error: "Error fetching data" });
-			}
+	console.log("Fetching duelists...");
+	var query = `SELECT Name FROM ${db_tables.duelist}`;
+	connection.query(query, (err, results, fields) => {
+		if (err) {
+			console.log(err);
+			connection.end();
+			return res.status(500).json({ error: "Error fetching data" });
+		}
 
-			const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
-			res.json(data);
-		});
-	} else {
-		console.log("fetch-dueslists timed out!");
-	}
+		const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
+		res.json(data);
+	});
 });
 
 app.post("/yugioh-fm-tools/api/fetch-card-drops", (req, res) => {
-	if (!req.timedout){
-		console.log("Fetching card drops...");
-		var query = `SELECT * FROM ${db_tables.card_drop} WHERE Duelist = ?`;
-		const { queryFilter } = req.body;
-		connection.query(query, [queryFilter], (err, results, fields) => {
-			if (err) {
-				connection.end();
-				return res.status(500).json({ error: "Error fetching data" });
-			}
-			const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
-			res.json(data);
-		});
-	} else {
-		console.log("fetch-card-drops timed out!");
-	}
+	console.log("Fetching card drops...");
+	var query = `SELECT * FROM ${db_tables.card_drop} WHERE Duelist = ?`;
+	const { queryFilter } = req.body;
+	connection.query(query, [queryFilter], (err, results, fields) => {
+		if (err) {
+			connection.end();
+			return res.status(500).json({ error: "Error fetching data" });
+		}
+		const data = JSON.parse(JSON.stringify(results)); // Parse the entire results array
+		res.json(data);
+	});
 });
 
 const PORT = process.env.PORT || 3002;
