92ebc935bfc3f364694ce1c083e6c0095666f675
hosua <hosua.o@gmail.com>
2025-01-08 23:07:57 -0500

    Fix vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/yugioh-fm-tools/api": {
-				target: "http://127.0.0.1:3002",
-				changeOrigin: true,
-				secure: false,
-			}
-		},
-	},
-})
+  base: "/yugioh-fm-tools/",
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/yugioh-fm-tools/api": {
+        target: "http://127.0.0.1:3002",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
