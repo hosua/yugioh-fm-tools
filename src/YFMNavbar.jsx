13aeb92599bcd8e5969bd526f4074d58663e9249
hosua <hosua.o@gmail.com>
2025-01-08 22:57:01 -0500

    Fix incorrect equips point yield in Calc

--- src/YFMNavbar.jsx
+++ src/YFMNavbar.jsx
@@ -1,33 +1,42 @@
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './YFM.css'
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./YFM.css";
 
 function YFMNavbar() {
-    return (
-        <>
-            <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand as={Link} to="/">Yu-Gi-Oh - FM Tools</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="yugioh-fm-tools/duel-calculator">Duel Points Calculator</Nav.Link>
-                            <Nav.Link as={Link} to="yugioh-fm-tools/duel-common-strats">Duel Common Strats</Nav.Link>
-                            <Nav.Link as={Link} to="yugioh-fm-tools/guardian-star">Guardian Star Chart</Nav.Link>
-                            {/* TODO: This won't work on gh-pages since it requires a server, so comment this out before deploying the app! */}
+  return (
+    <>
+      <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Yu-Gi-Oh - FM Tools
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="yugioh-fm-tools/duel-calculator">
+                Duel Points Calculator
+              </Nav.Link>
+              <Nav.Link as={Link} to="yugioh-fm-tools/duel-common-strats">
+                Duel Common Strats
+              </Nav.Link>
+              <Nav.Link as={Link} to="yugioh-fm-tools/guardian-star">
+                Guardian Star Chart
+              </Nav.Link>
+              {/* TODO: This won't work on gh-pages since it requires a server, so comment this out before deploying the app! */}
+              {/*
                             <NavDropdown title="Database">
                                 <NavDropdown.Item as={Link} to="yugioh-fm-tools/database/card-list">Card List</NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="yugioh-fm-tools/database/card-drops">Card Drops</NavDropdown.Item>
                             </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <br />
-        </>
-    );
+                            */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <br />
+    </>
+  );
 }
-
 
 export { YFMNavbar };
